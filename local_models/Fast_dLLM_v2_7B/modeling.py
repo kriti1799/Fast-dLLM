34733b8040a16a5e82a93b1ac75d8cfbdfec123d
kriti1799 <kriti.agrawal1799@gmail.com>
2026-02-01 15:08:39 -0500

    Changes

--- local_models/Fast_dLLM_v2_7B/modeling.py
+++ local_models/Fast_dLLM_v2_7B/modeling.py
@@ -500,7 +500,7 @@ class Fast_dLLM_QwenModel(Fast_dLLM_QwenPreTrainedModel):
         # create position embeddings to be shared across the decoder layers
         position_embeddings = self.rotary_emb(hidden_states, position_ids)
 
-        layer_cos_threshold = kwargs.pop("layer_cos_threshold", 0.99)
+        layer_cos_threshold = kwargs.pop("layer_cos_threshold", 0.9)
         mask_id = kwargs.pop("mask_id", 151665)
 
         prev_layer_in = None
@@ -576,7 +576,7 @@ class Fast_dLLM_QwenModel(Fast_dLLM_QwenPreTrainedModel):
         "ratio": float(skipped_layers) / max(1, int(eligible_layers)),
         "threshold": float(layer_cos_threshold),
         }
-        
+
         return BaseModelOutputWithPastAndBlockCache(
             last_hidden_state=hidden_states,
             past_key_values=past_key_values if use_cache else None,
