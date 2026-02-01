77995f850f152df0ae48212288e3a86f901e65d3
kriti1799 <kriti.agrawal1799@gmail.com>
2026-02-01 16:12:19 -0500

    Changes

--- local_models/Fast_dLLM_v2_7B/modeling.py
+++ local_models/Fast_dLLM_v2_7B/modeling.py
@@ -502,7 +502,7 @@ class Fast_dLLM_QwenModel(Fast_dLLM_QwenPreTrainedModel):
 
         layer_cos_threshold = kwargs.pop("layer_cos_threshold", 0.99)
         mask_id = kwargs.pop("mask_id", 151665)
-        # layer_cos_threshold = 0.99
+        layer_cos_threshold = 0.95
 
         prev_layer_in = None
         skipped_layers = 0
