95f92b507a2ec28857a8db51ec5f46f674a1a2c4
kriti1799 <kriti.agrawal1799@gmail.com>
2026-02-01 15:48:35 -0500

    Changes

--- v2/eval.py
+++ v2/eval.py
@@ -112,6 +112,7 @@ class Fast_dLLM_v2EvalHarness(LM):
         self.threshold = threshold
         self.bd_size = bd_size
         self.token_cos_threshold = token_cos_threshold
+        self.layer_cos_threshold =0.99
 
 
     @property
@@ -295,7 +296,7 @@ class Fast_dLLM_v2EvalHarness(LM):
                         use_block_cache=self.use_block_cache,
                         threshold=self.threshold,
                         token_cos_threshold=self.token_cos_threshold,
-                        layer_cos_threshold = self.layer_cos_threshold
+                        layer_cos_threshold = self.layer_cos_threshold,
                     )
 
                     m = self.accelerator.unwrap_model(self.model) if self.accelerator is not None else self.model
