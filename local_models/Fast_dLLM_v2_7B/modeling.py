15300125613f57a1d9894a1bbcf55a4395db8810
kriti1799 <kriti.agrawal1799@gmail.com>
2026-02-02 12:00:31 -0500

    Changed the if statement caches

--- local_models/Fast_dLLM_v2_7B/modeling.py
+++ local_models/Fast_dLLM_v2_7B/modeling.py
@@ -520,7 +520,7 @@ class Fast_dLLM_QwenModel(Fast_dLLM_QwenPreTrainedModel):
                 layer_idx > 0
                 and prev_layer_in is not None
                 and layer_cos_threshold <= 1.0
-                and (not update_past_key_values) and (not use_cache) and (not use_block_cache)
+                and (not update_past_key_values) 
                   
             ):
                 
